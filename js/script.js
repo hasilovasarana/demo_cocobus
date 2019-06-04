81e3c0bc1cc3b0928f0f97197f998fb4618106ae
hasilovasarana <saranakhh@code.edu.az>
2019-06-05 01:44:43 +0400

    added font awesome

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Responsive Side Menu Js
+
+// var res_nav =document.getElementById("responsive_side_menu").innerHTML;
+// if(document.getElementById("responsive_side_menu").style.minWidth == "1124 px"){
+//     document.getElementById("responsive_side_menu").style.visibility="block";
+// }else{
+//     document.getElementById("responsive_side_menu").style.visibility="none";
+// }
+
+
+// document.getElementById("responsive_side_menu_icon").style.visibility=""
+
+
+
+function hide_visible(){
+    document.getElementById("responsive_side_menu").style.visibility="visible";  
+
+}
